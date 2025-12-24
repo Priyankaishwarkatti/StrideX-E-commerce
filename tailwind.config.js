3e11cfccf66cf06169beb63ded12a9e295d42ae9
Priyanka Ishwarkatti <priyankaishwarkatti22@gmail.com>
2025-12-24 20:08:28 +0530

    Fix Tailwind build setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.html",
-    "./src/**/*.js",
-    "./public/**/*.html",
+    "./src/js/**/*.js",
+    "./src/components/**/*.html",
   ],
   theme: {
     extend: {
